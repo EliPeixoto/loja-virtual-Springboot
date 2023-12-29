820fb527a6b5f78d3c949edeb3a1a5d119d390b6
EliPeixoto <eliane.peixoto@outlook.com>
2023-12-29 16:45:30 -0300

    atualizando FuncionarioControle

--- src/main/java/com/dev/loja/controle/FuncionarioControle.java
+++ src/main/java/com/dev/loja/controle/FuncionarioControle.java
@@ -1,17 +1,19 @@
 package com.dev.loja.controle;
 
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.dev.loja.modelos.Funcionario;
-import com.dev.loja.repositorios.CidadeRepositorio;
 import com.dev.loja.repositorios.FuncionarioRepositorio;
 
 @Controller
@@ -20,8 +22,7 @@ public class FuncionarioControle {
 	@Autowired
 	private FuncionarioRepositorio funcionarioRepositorio;
 	
-	@Autowired
-	private CidadeRepositorio cidadeRepositorio;
+
 	
 	
 	@GetMapping("/administrativo/funcionarios/cadastrar")
@@ -38,7 +39,20 @@ public class FuncionarioControle {
 		return mv;
 	}
 	
+	@GetMapping("/administrativo/funcionarios/editar/{id}")
+	public ModelAndView editar(@PathVariable("id") Long id) {
+		Optional<Funcionario>funcionario = funcionarioRepositorio.findById(id);
+		return cadastrar(funcionario.get());
+	}
 
+	@GetMapping("/administrativo/funcionarios/remover/{id}")
+	public ModelAndView remover(@PathVariable("id") Long id) {
+		Optional<Funcionario>funcionario = funcionarioRepositorio.findById(id);
+		funcionarioRepositorio.delete(funcionario.get());
+		return listar();
+	}
+
+	
 	@PostMapping("/administrativo/funcionarios/salvar")
 	public ModelAndView salvar(@Valid Funcionario funcionario, BindingResult result) {
 		if(result.hasErrors()) {
