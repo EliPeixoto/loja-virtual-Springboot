cf6d0073158c813a7af29038499c4780e4de97df
EliPeixoto <eliane.peixoto@outlook.com>
2024-01-04 11:16:10 -0300

    configurando persistencia

--- src/main/java/com/dev/loja/DataConfiguration.java
+++ src/main/java/com/dev/loja/DataConfiguration.java
@@ -13,23 +13,25 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 public class DataConfiguration {
 
 	@Bean
-	public DataSource dataSource() {
+	public DataSource dataSource(){
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/loja?useTimezone=true&serverTimezone=America/Sao_Paulo");
 		dataSource.setUsername("root");
-		dataSource.setPassword("");
+		dataSource.setPassword("");		
 		return dataSource;
+	
 	}
 	
-	@Bean
 	public JpaVendorAdapter jpaVendorAdapter() {
 		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
 		adapter.setDatabase(Database.MYSQL);
 		adapter.setShowSql(true);
 		adapter.setGenerateDdl(true);
 		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
-		adapter.setPrepareConnection(true);
+		adapter.setPrepareConnection(true);		
 		return adapter;
 	}
+	
+
 }
