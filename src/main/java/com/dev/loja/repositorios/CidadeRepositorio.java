56e8aefda94ad8627964ec2e38a320f59aaa3b68
EliPeixoto <eliane.peixoto@outlook.com>
2024-01-04 11:02:17 -0300

    deixando projeto do inicio para começar praticar

--- src/main/java/com/dev/loja/repositorios/CidadeRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.dev.loja.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dev.loja.modelos.Cidade;
-
-public interface CidadeRepositorio extends JpaRepository<Cidade, Long> {
-
-}
